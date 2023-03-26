edfb8f489049dbd48f390ca1fedfeaed7c50c1fa
seidyimer <seiyim79@gmail.com>
2023-03-26 08:28:08 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return: 0
  */
 int main(void)
-{       
-        puts("\"programming is like building a multilingual puzzle");
-        return (0);
-}  
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
