906f778e2d0574e7e08df4735b8ecd9c8e2249ce
seidyimer <seiyim79@gmail.com>
2023-03-27 08:43:38 -0700

    main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a. int *b);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -17,4 +17,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 #endif 
- 
+  
